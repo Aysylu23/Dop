e4fdaba6a97528ced835cc36d6df21da290ca022
Aysylu <aysylu_bad@mail.ru>
2023-11-11 15:00:11 +0500

    add task2

---
+++ Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿/*Eсть программа с бесконечным циклом. 
+Когда пользователь вводит exit программа заканчивается*/
+
+
+/*Console.Write("Введите слово: ");
+
+string word = Console.ReadLine();
+
+for(; ;)
+{
+    Console.Write("hello");
+    if(word == "exit")
+    {
+        break;
+    }
+}*/
+
+
+string someMsg = ReadInput("Введите кодовое слово: ");
+string acsessCode = "exit";
+
+while (someMsg != acsessCode)
+{
+    someMsg = ReadInput("Введите кодовое слово: ");
+}
+
+
+// Функция ввода
+string ReadInput(string msg)
+{
+    Console.Write(msg);
+    return Console.ReadLine();
+}   
+
+
+
+
+
+
+
+
+
