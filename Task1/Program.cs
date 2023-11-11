abe2283eb55347aedf61389056ffebbadf38c374
Aysylu <aysylu_bad@mail.ru>
2023-11-11 15:03:12 +0500

    add task1_1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,7 +1,7 @@
 ﻿/*В переменной string есть секретное сообщение, во второй есть пароль. 
 Пользователю программы даётся 3 попытки ввести пароль и увидеть секретное сообщение.*/
 
-Console.WriteLine("Введите пароль. У вас есть 3 попытки.");
+/*Console.WriteLine("Введите пароль. У вас есть 3 попытки.");
 string password1 = Console.ReadLine();
 
 
@@ -32,6 +32,35 @@ else
         else
         {
             Console.WriteLine("К сожалению ваши попытки закончились. До свидания.");
+        }
+    }
+}*/
+
+
+Console.WriteLine("У вас есть 3 попытки.");
+string input_password = "A35g!";
+string message = "Добро пожаловать!";
+Console.WriteLine("Введите пароль: ");
+
+for (int i = 1; i <= 3; i++)
+{
+
+    string password = Console.ReadLine();
+
+    if (password == input_password)
+    {
+        Console.WriteLine($"{message}");
+        break;
+    }
+    else
+    {
+        if (i < 3)
+        {
+            Console.WriteLine("Вы ввели неверный пароль. Попробуйте снова: ");
+        }
+        else
+        {
+            Console.WriteLine("К сожалению, ваши попытки закончились.");
         }
     }
 }
